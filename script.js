c8780413bd28c77d3959f8f2ace6a62769fc5e00
dulovici <igorx24@gmail.com>
2021-09-26 20:06:08 +0200

    v1.0

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('dev')
+
+(function() {
+'use strict'
+
+const inputs = [...document.querySelectorAll('.input')];
+const email = document.querySelector('.email');
+const registerButton = document.querySelector('.register');
+
+function checkEmail() {
+    const re = /\S+@\S+\.\S+/;
+    re.test(email.firstElementChild.value) 
+    ? email.classList.remove('show')
+    : email.classList.add('show');
+}
+
+function checkInputs() {
+    inputs.forEach(el => {
+        el.firstElementChild.value.length === 0 ? el.classList.add('show') : el.classList.remove('show');
+        checkEmail()
+    })
+
+}
+
+registerButton.addEventListener('click', checkInputs)
+})()
