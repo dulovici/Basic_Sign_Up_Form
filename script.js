48769333cb67996bdb5e588fd56a52ddbb252f65
dulovici <81692156+dulovici@users.noreply.github.com>
2021-09-30 10:28:19 +0200

    Dev (#1)
    * dev init
    
    * desktop layout
    
    * mobile layout
    
    * v1.0
    
    * terms and services :hover
    
    * email place holder + some small estetic changes
    
    * padding corection mobile
    
    * code refactor v2.0

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('main')
+
+(function() {
+'use strict'
+
+const inputs = [...document.querySelectorAll('.input')];
+const email = document.querySelector('.email');
+const registerButton = document.querySelector('.register');
+
+function checkEmail() {
+    const re = /\S+@\S+\.\S+/;
+    re.test(email.firstElementChild.value) 
+    ? email.classList.remove('show')
+    : email.classList.add('show');
+
+    email.firstElementChild.placeholder = 'email@example/com';
+}
+
+function checkInputs() {
+    inputs.forEach(el => {
+        el.firstElementChild.value.length === 0 ? el.classList.add('show') : el.classList.remove('show');
+        checkEmail()
+    })
+
+}
+
+registerButton.addEventListener('click', checkInputs)
+
+})()
+
